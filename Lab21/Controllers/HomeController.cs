bfc4c785024824df9f541590b87bb0bc9c19f3d8
Anthony <anth0ny0153@gmail.com>
2018-05-24 23:28:48 -0400

    first commit for lab21

---
+++ Lab21/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab21.Models;
+namespace Lab21.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult SignUpForm()
+        {
+            return View();
+        }
+        public ActionResult AddNewUser(UserInfo newUser)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Message = $"Welcome {newUser.FirstName}";
+                return View("Confirm");
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+        public ActionResult Coffee()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lab21/Models/UserInfo.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab21.Models
+{
+    public class UserInfo
+    {
+        private string firstName;
+        private string lastName;
+        private string email;
+        private string phoneNumber;
+        private string password;
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastName; 
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
+        public string PhoneNumber
+        {
+            get
+            {
+                return phoneNumber;
+            }
+            set
+            {
+                phoneNumber = value;
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                password = value;
+            }
+        }
+        public UserInfo()
+        {
+
+        }
+        public UserInfo(string firstName, string lastName, string email, string phoneNumber, string password)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            Password = password;
+        }
+    }
+}
